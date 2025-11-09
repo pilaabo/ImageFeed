5e264f85e42edcdfa467e3aa1edc99997dca8e8d
Waldemar <vladdigen@icloud.com>
2025-11-09 18:04:43 +0400

    feat(ui): add dynamic cell height calculation based on image aspect ratio

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,38 +1,46 @@
 import UIKit
 
 class ImagesListCell: UITableViewCell {
+    
+    // MARK: - Reuse Identifier
+    
     static let reuseIdentifier = "ImagesListCell"
     
-    @IBOutlet private weak var cellImage: UIImageView!
-    @IBOutlet private weak var dateLabel: UILabel!
-    @IBOutlet private weak var likeButton: UIButton!
-    @IBOutlet private weak var gradientView: UIView!
+    // MARK: - Outlets
+    
+    @IBOutlet weak var cellImage: UIImageView?
+    @IBOutlet weak var dateLabel: UILabel?
+    @IBOutlet weak var likeButton: UIButton?
+    @IBOutlet private weak var gradientView: UIView? // private, не используется извне
+    
+    // MARK: - Properties
     
     private let gradientLayer = CAGradientLayer()
-
+    
+    // MARK: - Lifecycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setupGradient()
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
+        guard let gradientView else { return }
         gradientLayer.frame = gradientView.bounds
     }
-
+    
+    // MARK: - Private Methods
+    
     private func setupGradient() {
         let transparent = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0).cgColor
         let opaque = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1).cgColor
-
-        gradientLayer.colors = [
-            transparent,
-            opaque
-        ]
+        
+        gradientLayer.colors = [transparent, opaque]
         gradientLayer.locations = [0.0, 1.0]
-
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
         gradientLayer.endPoint   = CGPoint(x: 0.5, y: 1.0)
-
-        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        gradientView?.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,26 +1,45 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Vladimir Generalov on 02.11.2025.
-//
-
 import UIKit
 
 class ImagesListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView?
     
+    private let photosNames = Array(0..<20).map { "\($0)" }
+    
+    // MARK: - Date Formatter
+    
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.dateFormat = "d MMMM yyyy"
+        return formatter
+    }
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView?.rowHeight = 200
-
-        // Do any additional setup after loading the view.
     }
 }
 
+// MARK: - Configuration
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosNames[indexPath.row]) else { return }
+        
+        cell.cellImage?.image = image
+        cell.dateLabel?.text = dateFormatter.string(from: Date())
+        
+        let likeState = indexPath.row.isMultiple(of: 2) ? "Liked" : "Not Liked"
+        cell.likeButton?.setImage(UIImage(named: likeState), for: .normal)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        photosNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -28,13 +47,25 @@ extension ImagesListViewController: UITableViewDataSource {
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
         return cell
     }
-    
-    func configCell(for cell: ImagesListCell) {}
 }
+
+// MARK: - UITableViewDelegate
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosNames[indexPath.row]) else { return 0 }
+        
+        let imageWidth = image.size.width
+        let imageHeight = image.size.height
+        let tableWidth = tableView.bounds.width
+        
+        let scaledHeight = imageHeight * (tableWidth / imageWidth)
+        
+        return scaledHeight
+    }
 }
